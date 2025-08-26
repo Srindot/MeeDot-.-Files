b5b9e3cb1970704f9acda1fec76491126c6f3004
Srindot <srinath.bhamidipati@research.iiit.ac.in>
2025-08-27 03:08:01 +0530

    updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,10 @@
 require("srindot")
+-- Copy selected text to system clipboard using wl-copy
+vim.keymap.set("v", "<leader>y", ":w !wl-copy<CR><CR>", { silent = true })
+
+-- Copy entire file to clipboard
+vim.keymap.set("n", "<leader>Y", ":%w !wl-copy<CR><CR>", { silent = true })
+
+-- Paste from clipboard into insert mode
+vim.keymap.set("n", "<leader>p", ":r !wl-paste<CR>", { silent = true })
+
