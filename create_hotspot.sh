c4b7f07c7f13b07da1b778a9d2649cc276171015
Srindot <srinath.bhamidipati@research.iiit.ac.in>
2026-01-28 17:43:43 +0530

    Updates in the projects structure

--- create_hotspot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create a Wi-Fi hotspot with the updated settings and specify IP range
-nmcli dev wifi hotspot ifname wlan0 ssid stimp password alpha_12 
-# Show the password for the hotspot
-nmcli dev wifi show-password
-
